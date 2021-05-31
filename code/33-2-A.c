59964cbd23aae5ed2f90fb1ae3e1d036c7365d02
“yourname” <82037182@qq.com>
2021-05-31 23:11:04 +0800

    第34课 - 多维数组和多维指针

--- code/33-2-A.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-    printf("I'm A!\n");
-    return 0;
-}
-
-
--- code/33-2-B.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-    printf("I'm B!\n");
-    return 99;
-}
-
-
