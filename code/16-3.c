63835b3d58d579ad9e88216008a08ba065b22659
“yourname” <82037182@qq.com>
2021-05-31 22:34:55 +0800

    第16课 - 位运算符分析

--- code/16-3.c
+++ code/16-3.c
@@ -6,11 +6,10 @@ int main()
     int j = 0;
     int k = 0;
     
-    ++i || ++j && ++k;
-
-    printf("%d\n", i);
-    printf("%d\n", j);
-    printf("%d\n", k);
+    if( ++i | ++j & ++k )
+    {
+        printf("Run here...\n");
+    }
     
     return 0;
 }
